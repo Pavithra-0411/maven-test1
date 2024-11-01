66789eee2c99f4ea2c9ffb30e5a918f0427aba09
Pavithra-0411 <cherukupallypavithrayadav2019@gmail.com>
2024-11-01 23:37:54 +0530

    PAVITHRA_C_0001- Wrote the code

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,12 @@
+package com.example.javamavenjunithehelloworld;
+
+public class Utility
+{
+    public static String oddOrEven(int n)
+    {
+        return (n%2==0)?"Even":"Odd";
+    }
+	public static pavithra {
+		System.out.println("Hello World");
+	}
+}
